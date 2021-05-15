c152b57097e5222f5f1d0dc1a19e5716d16b1d67
gartnerleandro <gartnerleandro@gmail.com>
2021-05-15 17:03:02 +0200

    fix: missing function

--- src/pickers/SlidersColorPicker.js
+++ src/pickers/SlidersColorPicker.js
@@ -100,7 +100,7 @@ export class SlidersColorPicker extends Component {
             <View style={styles.colorString}>
               <TextInput
                 value={modes[this.state.mode].getString(this.state.color)}
-                onChangeText={}
+                onChangeText={updateHex}
                 style={styles.colorStringText}
               />
             </View>
